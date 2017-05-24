3ffd4f73ef9ef583c082a980ebf0e2f8c00e5ed8
William Thomas <wthoma390@gmail.com>
2017-05-24 17:54:55 -0600

    pass user can log in test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,14 @@ class SessionsController < ApplicationController
 
   end
 
+  def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = "Successfully logged in!"
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
 end
