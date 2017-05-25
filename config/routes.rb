76cd5245ecd419fe1501efb7eaa75b981139d8d3
Rhonda <rwilhel@gmail.com>
2017-05-25 10:06:25 -0600

    Creates feature test for comment functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
 
   resources :users
-  
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :movies, only: [:show] do
+    resources :comments, only: [:create]
+  end
+
+  resources :movies
 end
---
+++ spec/features/users/user_can_leave_movie_comment_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "User can create a comment" do
+  scenario "a user can create a comment on a movie's page" do
+    movie = create(:movie)
+    user = create(:user)
+
+    visit movie_path(movie)
+
+    fill_in "comment[content]", with: "*DOUBLE ROCK HAND EMOJI*"
+    click_on "Create Comment"
+    expect(current_path).to eq movie_path(movie)
+    expect(page).to have_content("*DOUBLE ROCK HAND EMOJI*")
+    expect(page).to have_content(user.username)
+  end
+end
