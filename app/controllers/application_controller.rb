a0884bdaefe2c34cb6048f916f8e480e2463403e
William Thomas <wthoma390@gmail.com>
2017-05-28 15:31:27 -0600

    swithced guidebox model to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :movie
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def guidebox
+    guidebox = GuideboxWrapper::GuideboxMovie.new("0a5a2b426dfeaec79af26be75dfa937ad786cee5", "US")
+  end
+
+  def movie(movie_title)
+    guidebox.fetch_movie(movie_title)
+  end
+
 end
--- app/models/guidebox.rb
+++ app/models/guidebox.rb
@@ -1,8 +1,11 @@
-class GuideBox < ApplicationRecord
-
-  def movie(film_title)
-    guidebox = GuideboxWrapper::GuideboxMovie.new("0a5a2b426dfeaec79af26be75dfa937ad786cee5", "US")
-    guidebox.fetch_movie(film_title)
-  end
-
-end
+# class GuideBox < ApplicationRecord
+#
+#   def guidebox
+#     guidebox = GuideboxWrapper::GuideboxMovie.new("0a5a2b426dfeaec79af26be75dfa937ad786cee5", "US")
+#   end
+#
+#   def movie(movie_title)
+#     guidebox.fetch_movie(movie_title)
+#   end
+#
+# end
