18f5757912c321c8cd8afec7a1fecbaed8abe736
Rhonda <rwilhel@gmail.com>
2017-05-28 20:16:10 -0600

    Adds style to application and new user form

--- spec/features/users/visitor_can_create_account_spec.rb
+++ spec/features/users/visitor_can_create_account_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 RSpec.describe "User visits a new user page" do
   it "they can create a new user" do
     visit new_user_path
-    
+
+    expect(page).to have_content("Create A New Account!")
+
     fill_in "user[username]", with: "zuzu"
     fill_in "user[email]", with: "zuzu@pup-tart.com"
     fill_in "user[password]", with: "meatball"
-    click_on "Create User Account"
+    click_on "Create Account"
 
     expect(current_path).to eq(user_path(User.last.id))
     expect(page).to have_content("Successfully logged in!")
